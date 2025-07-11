c33fcdabfd136f264c865b751b29b4aebb34fb45
Nimira43 <n.rabone72@aol.com>
2025-07-12 00:30:39 +0100

    All routing done for now

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ import SignUp from '@/views/auth/SignUp.vue'
 import ShoppingCart from '@/views/cart/ShoppingCart.vue'
 import Home from '@/views/home/Home.vue'
 import MenuItemList from '@/views/menu-item/MenuItemList.vue'
-import MenuItemUpsert from '@/views/menu-item/MenuItemList.vue'
+import MenuItemUpsert from '@/views/menu-item/MenuItemUpsert.vue'
 import OrderConfirmation from '@/views/order/OrderConfirmation.vue'
 import OrderHistoryList from '@/views/order/OrderHistoryList.vue'
 import OrderManagement from '@/views/order/OrderManagement.vue'
@@ -36,34 +36,40 @@ const router = createRouter({
       component: SignUp
     },
     {
-      path: '/',
-      name: APP_ROUTE_NAMES.HOME,
-      component: Home
+      path: '/cart',
+      name: APP_ROUTE_NAMES.CART,
+      component: ShoppingCart
     },
     {
-      path: '/',
-      name: APP_ROUTE_NAMES.HOME,
-      component: Home
+      path: '/admin/manage-menu-items',
+      name: APP_ROUTE_NAMES.MENU_ITEM_LIST,
+      component: MenuItemList
     },
     {
-      path: '/',
-      name: APP_ROUTE_NAMES.HOME,
-      component: Home
+      path: '/admin/manage-menu-items/create',
+      name: APP_ROUTE_NAMES.CREATE_MENU_ITEM,
+      component: MenuItemUpsert
     },
     {
-      path: '/',
-      name: APP_ROUTE_NAMES.HOME,
-      component: Home
+      path: '/admin/manage-menu-items/update/:id',
+      name: APP_ROUTE_NAMES.EDIT_MENU_ITEM,
+      component: MenuItemUpsert,
+      props: true
     },
     {
-      path: '/',
-      name: APP_ROUTE_NAMES.HOME,
-      component: Home
+      path: '/admin/order-confirmation/:orderId',
+      name: APP_ROUTE_NAMES.ORDER_CONFIRM,
+      component: OrderConfirmation
     },
     {
-      path: '/',
-      name: APP_ROUTE_NAMES.HOME,
-      component: Home
+      path: '/order-list',
+      name: APP_ROUTE_NAMES.ORDER_LIST,
+      component: OrderHistoryList
+    },
+    {
+      path: 'admin/order-management',
+      name: APP_ROUTE_NAMES.MANAGE_ORDER_ADMIN,
+      component: OrderManagement
     },
     {
       path: '/:catchAll(.*)',
