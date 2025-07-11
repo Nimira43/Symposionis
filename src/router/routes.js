019cc1fdc5594fea0aab950af200dacd5d7e06bc
Nimira43 <n.rabone72@aol.com>
2025-07-11 23:58:13 +0100

    Importing first lot of views into routes.js

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,11 @@
+import NoAccess from '@/views/auth/NoAccess.vue'
+import NotFound from '@/views/auth/NotFound.vue'
+import SignIn from '@/views/auth/SignIn.vue'
+import SignUp from '@/views/auth/SignUp.vue'
+import ShoppingCart from '@/views/cart/ShoppingCart.vue'
+import Home from '@/views/home/Home.vue'
+import menu
+
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
