c6cc1673c4693ac88fc429a59be1addb2ed9739a
Nimira43 <n.rabone72@aol.com>
2025-07-12 00:00:39 +0100

    Completed those imports

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,11 @@ import SignIn from '@/views/auth/SignIn.vue'
 import SignUp from '@/views/auth/SignUp.vue'
 import ShoppingCart from '@/views/cart/ShoppingCart.vue'
 import Home from '@/views/home/Home.vue'
-import menu
+import MenuItemList from '@/views/menu-item/MenuItemList.vue'
+import MenuItemUpsert from '@/views/menu-item/MenuItemList.vue'
+import OrderConfirmation from '@/views/order/OrderConfirmation.vue'
+import OrderHistoryList from '@/views/order/OrderHistoryList.vue'
+import OrderManagement from '@/views/order/OrderManagement.vue'
 
 import { createRouter, createWebHistory } from 'vue-router'
 
