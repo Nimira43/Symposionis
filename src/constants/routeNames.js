d619fa93aea2416cbd7529289b879ab2f99e09e6
Nimira43 <n.rabone72@aol.com>
2025-07-11 23:44:58 +0100

    Creating object to hold route names

---
+++ src/constants/routeNames.js
@@ -0,0 +1,14 @@
+export const APP_ROUTE_NAMES = Object.freeze({
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+  HOME: '',
+})
