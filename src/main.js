312b8a2c98c6c058585d0e0022335c796f33b3b8
Nimira43 <n.rabone72@aol.com>
2025-07-22 23:46:43 +0100

    Created main.css - mostly to hold all the custom CSS that'll be used alongside Bootstrap.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
+import './main.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router/routes'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
 
