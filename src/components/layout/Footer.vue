130064c7c287c48bd518d83b73fc0c439decf998
Nimira43 <n.rabone72@aol.com>
2025-07-22 23:23:48 +0100

    Completed simple footer.

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,1 +1,7 @@
-<template>Footer</template>
+<template>
+
+  <div class="footer text-center mx-auto mt-4 p-3 bg-success rounded-3">
+    Symposionis &copy; 2025. Designed by NimiraTech.
+  </div>
+
+</template>
