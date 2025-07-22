df0e99889fe55da5dd25bb31248f340501186415
Nimira43 <n.rabone72@aol.com>
2025-07-23 00:02:56 +0100

    Removed unwanted import

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,6 @@
 <script setup>
 import Header from './components/layout/Header.vue'
 import Footer from './components/layout/Footer.vue'
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
