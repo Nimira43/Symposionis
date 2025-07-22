15240602141cc4888404fc9d633d64c4d40515aa
Nimira43 <n.rabone72@aol.com>
2025-07-23 00:32:20 +0100

    Brought in logo and logo text

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,5 +1,16 @@
 <template>
   <nav class="navbar navbar-expand-md mx-auto my-2 w-100 bg-grey-light-extra bg-opacity-10 text-grey-dark">
-    
+    <div class="container-fluid">
+      <router-link class="navbar-brand" href="#">
+        <img 
+          src="@/assets/logo.png" 
+          alt="Unicorn logo"
+          width="30"
+          height="30"
+          class="d-inline-block align-text-top me-2"
+        > 
+        <span class="logo">Symposionis</span>       
+      </router-link>
+    </div>
   </nav>
 </template>
