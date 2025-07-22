6cd0480e513742129b3e67c7183e4ea31e5259cf
Nimira43 <n.rabone72@aol.com>
2025-07-23 00:44:29 +0100

    Button

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,20 +1,33 @@
 <template>
   <nav class="navbar navbar-expand-md mx-auto my-2 w-100 bg-grey-light-extra bg-opacity-10 text-grey-dark">
     <div class="container-fluid">
-      <router-link class="navbar-brand" href="#">
+      <router-link 
+        class="navbar-brand" 
+        :to="{name: APP_ROUTE_NAMES.HOME}"
+      >
         <img 
           src="@/assets/logo.png" 
           alt="Unicorn logo"
           width="30"
           height="30"
           class="d-inline-block align-text-top me-2"
-        > 
-        <span class="logo">Symposionis</span>       
+        >        
       </router-link>
+      <button
+        class="navbar-toggler bg-light"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler icon text-light"></span>
+      </button>
     </div>
   </nav>
 </template>
 
-<script>
+<script setup>
 import { APP_ROUTE_NAMES } from '@/constants/routeNames'
 </script>
