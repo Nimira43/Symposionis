5b3dd7f217fe7a812fd5f66590f625822054675b
Nimira43 <n.rabone72@aol.com>
2025-08-04 16:40:17 +0100

    import piniaPluginPersistedstate

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import router from './router/routes'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+
+
 
 const app = createApp(App)
 
